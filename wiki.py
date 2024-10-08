0904eafe6a2fcc0873db5d62999d907f6f04f847
Maria Karapetyan <mkartist528@gmail.com>
2024-10-08 18:02:19 +0000

    it works now

--- wiki.py
+++ wiki.py
@@ -1,3 +1,4 @@
+from queue import Queue
 import wikipediaapi
 import time
 
@@ -14,16 +15,44 @@ def fetch_links(page):
     return links_list
 
 def wikipedia_game_solver(start_page, target_page):
-    links = fetch_links(start_page)
-    
-    for link in links:
-        if wiki.page(link) == target_page:
-            print("We have found the page you're looking for!")
-        if link != target_page:
-            print("")
+    print("working on it...")
+    start_time = time.time()
 
-start_page = wiki.page("Sabrina Carpenter")
-target_page = wiki.page("Skims")
-wikipedia_game_solver(start_page, target_page)
+    queue = Queue()
+    visited = set()
+    parent = {}
 
-print(fetch_links(start_page))
+    queue.put(start_page.title)
+    visited.add(start_page.title)
+
+    while not queue.empty():
+        current_page_title = queue.get()
+        if current_page_title == target_page.title:
+            break
+
+        current_page = wiki.page(current_page_title)
+        links = fetch_links(current_page)
+
+        for link in links:
+            if link not in visited:
+                queue.put(link)
+                visited.add(link)
+                parent[link] = current_page_title
+
+    path = []
+    page_title = target_page.title
+    while page_title != start_page.title:
+        path.append(page_title)
+        page_title = parent[page_title]
+
+    path.append(start_page.title)
+    path.reverse()
+
+    end_time = time.time()
+    print("This algorithm took", end_time - start_time, "seconds.")
+    return path
+
+start_page = wiki.page("Pasadena High School (California)")
+target_page = wiki.page("World War II")
+path = wikipedia_game_solver(start_page, target_page)
+print(path)
